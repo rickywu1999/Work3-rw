027a7e75a0302678de81d9cac44fe1b0a8f43133
Ricky Wu <rickywu1999@gmail.com>
2016-10-26 22:30:31 -0400

    added errno stuff

--- random.c
+++ random.c
@@ -6,15 +6,26 @@
 #include <string.h>
 #include <sys/stat.h>
 
+
 int myrand(){
+    //OPEN
   int f = open("/dev/random",O_RDONLY);
+  if (f< 0){
+    printf("Failed to open file, error: %s\n",strerror(errno));
+    return -1;
+  }
   void * p = malloc(sizeof(int));
-  read(f,p,sizeof(int));
-  //printf("%d\n",*((int*)p));
+    //READ
+  if (read(f,p,sizeof(int)) < 0){
+    printf("Failed to read file, error: %s\n",strerror(errno));
+    return -1;
+  }
+
   srand(*((int*)p));
   free(p);
   return rand();
 }
+
 
 int main(){
   int arr[10];
@@ -22,28 +33,42 @@ int main(){
   printf("Generating Numbers:\n");
   while (c<10){
     int n = myrand();
+    if (n < 0){
+        return 0;
+    }
     arr[c] = n;
     printf("random#%d - %d\n",c+1,n );
     c++;
   }
+
   printf("\nWriting into randomList \n");
-  int fd = open("randomList",O_CREAT|O_RDWR|O_APPEND|O_EXCL,0644);
-  write(fd,arr,sizeof(int)*10);
-  /*write(fd,arr,40);
-    int arr2[10];
-    int d = 0;
-    read(fd,arr2,40);
-    while (d<10){
-        printf("%d\n",arr2[d]);
-        d++;
-    } */
+    //OPEN
+  int fd = open("randomList",O_CREAT|O_RDWR|O_TRUNC,0644);
+  if (fd< 0){
+    printf("Failed to open file, error: %s\n",strerror(errno));
+    return 0;
+  }
+  //WRITE
+  if(write(fd,arr,sizeof(int)*10) < 0){
+    printf("Failed to write to file, error %s\n",strerror(errno));
+    return 0;
+  }
   close(fd);
   printf("\nWriting Complete\n");
 
   printf("Reading randList into new array\n");
   int arr2[10];
+    //OPEN
   int nf = open("randomList", O_RDONLY);
-  read(nf, arr2, sizeof(int)*10);
+  if (nf< 0){
+    printf("Failed to open file, error: %s\n",strerror(errno));
+    return 0;
+  }
+    //READ
+  if (read(nf, arr2, sizeof(int)*10)< 0){
+    printf("Failed to read file, error: %s\n",strerror(errno));
+    return 0;
+  }
   close(nf);
 
   printf("Result from read:\n");
